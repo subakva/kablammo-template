4f127adace0942c9389bfec7de44a28f994c373f
Jason Wadsworth <jdwadsworth@gmail.com>
2013-09-21 09:55:33 -0700

    Update strategy.rb

--- strategy.rb
+++ strategy.rb
@@ -30,8 +30,58 @@ module Aggressive
   end
 end
 
-include Aggressive
+module Defensive
+  def dance
+    first_possible_move %w(n s e w).shuffle
+  end
+
+  def dodge(enemy)
+    toward = moves_toward enemy
+    d1 = enemy.distance_to robot.target_for(toward[1])
+    d2 = enemy.distance_to robot.target_for(toward[2])
+    if d1 > d2
+      moves = [ toward[1], toward[2], toward[3], toward[0] ]
+    else
+      moves = [ toward[2], toward[1], toward[3], toward[0] ]
+    end
+    first_possible_move moves
+  end
+
+  def act_defensively
+    enemy = opponents.first
+    return dance unless enemy
+    return dodge enemy if enemy.can_fire_at? me
+    return rest unless my.ammo_full?
+    move_away_from! enemy
+  end
+end
+
+extend Aggressive
+extend Defensive
+
+@current_target = nil
+
+def killrobotkill(robot, target)
+  if robot.can_fire_at?(target)
+    fire!
+  else
+    rotate!(robot.direction_to(target).round)
+  end
+end
 
 on_turn do
-  act_aggressively
+  if robot.ammo < 0
+    rest
+  elsif @current_target && @current_target.alive? && robot.can_see?(@current_target)
+    killrobotkill(robot, @current_target)
+  else
+    @current_target = opponents.first
+    if @current_target
+      killrobotkill(robot, @current_target)
+    elsif robot.ammo_full?
+      hunt
+    else
+      rest
+    end
+  end
 end
